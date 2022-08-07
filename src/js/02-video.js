4e84a8e873ebee16360d645d3c900f7f98c9a8e6
DenisVoron <Denis.voronko@gmail.com>
2022-08-07 22:32:47 +0300

    make task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Vimeo from '@vimeo/player';
 
+console.log(Vimeo);
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+player.on('play', function (time) {
+    console.log('played the video!');
+    console.log(time);
+});
+
+player.getVideoTitle().then(function (title) {
+    console.log('title:', title);
+});
+
+//player.getVideoTime
