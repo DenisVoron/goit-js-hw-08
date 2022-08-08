a194b08316e1ad71ec34ca805b22d66c6be7f54d
DenisVoron <Denis.voronko@gmail.com>
2022-08-08 14:29:32 +0300

    make task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,50 @@
 import Vimeo from '@vimeo/player';
-
 console.log(Vimeo);
+
+let PlayData;
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
 
-player.on('play', function (time) {
-    console.log('played the video!');
-    console.log(time);
+
+player.on('play', onPlay);
+
+function onPlay(data) {
+    //console.log(data);
+    //console.log(FormData);
+    //const objectPlay = JSON.stringify(data);
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+
+    const savedTime = localStorage.getItem('videoplayer-current-time');
+
+    const parsSavedTime = JSON.parse(savedTime);
+    //console.log(parsSavedTime);
+    PlayData = parsSavedTime;
+    console.log(PlayData);
+    //console.log(objectData);
+}
+
+
+player.setCurrentTime(PlayData).then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function (error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
 });
 
-player.getVideoTitle().then(function (title) {
-    console.log('title:', title);
-});
 
-//player.getVideoTime
+/*function returnTimeData() {
+    const savedTime = localStorage.getItem('time');
+
+    const parsSavedTime = JSON.parse(savedTime);
+    console.log(parsSavedTime);
+
+    PlayData = parsSavedTime;
+}*/
