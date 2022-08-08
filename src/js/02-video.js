aada27d75dd805075a25ecbaf5c06dd8da74289f
DenisVoron <Denis.voronko@gmail.com>
2022-08-08 23:22:34 +0300

    make task3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,11 @@
 import Vimeo from '@vimeo/player';
 console.log(Vimeo);
 
-let PlayData;
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
 
+let playData;
 
 player.on('play', onPlay);
 
@@ -13,20 +13,27 @@ function onPlay(data) {
     //console.log(data);
     //console.log(FormData);
     //const objectPlay = JSON.stringify(data);
-    localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data.seconds));
 
     const savedTime = localStorage.getItem('videoplayer-current-time');
 
     const parsSavedTime = JSON.parse(savedTime);
     //console.log(parsSavedTime);
-    PlayData = parsSavedTime;
-    console.log(PlayData);
+
+    playData = parsSavedTime;
+
+    /*if (parsSavedTime) {
+        playData = parsSavedTime;
+    }*/
+
+
     //console.log(objectData);
 }
+console.log(playData);
 
-
-player.setCurrentTime(PlayData).then(function (seconds) {
+player.setCurrentTime(playData).then(function (seconds) {
     // seconds = the actual time that the player seeked to
+    console.log(seconds);
 }).catch(function (error) {
     switch (error.name) {
         case 'RangeError':
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,65 @@
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    //input: document.querySelector('input'),
+    //textarea: document.querySelector('textarea')
+}
+
+//const form = document.querySelector('.feedback-form');
+
+const FormData = {};
+
+refs.form.addEventListener('submit', onFormSabmit);
+refs.form.addEventListener('input', onTextareaInput);
+//refs.textarea.addEventListener('input', onTextareaInput);
+
+function onTextareaInput(evt) {
+    const data = evt.target.value;
+    //console.log(data);
+    //const email = evt.target.value;
+    //console.log(email);
+    localStorage.setItem('feedback-form-state', data);
+}
+
+
+populateTextarea()
+
+refs.form.addEventListener('input', (e) => {
+    //console.log(e.target.name);
+    //console.log(e.target.value);
+    FormData[e.target.name] = e.target.value;
+    //console.log(FormData);
+    const objectData = JSON.stringify(FormData);
+    localStorage.setItem('feedback-form-state', objectData)
+    console.log(localStorage);
+    console.log(refs.form.email);
+
+    /*const saveData = localStorage.getItem('feedback-form-state');
+    const saveValue = JSON.parse(saveData);
+
+    if (saveValue) {
+        console.log(saveValue);
+
+        refs.form.email = saveValue;
+        refs.form.message = saveValue;
+    }*/
+})
+
+function onFormSabmit(evt) {
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    console.log('Отправляем форму');
+    localStorage.removeItem('feedback-form-state');
+}
+
+function populateTextarea() {
+    const saveData = localStorage.getItem('feedback-form-state');
+    //const saveValue = JSON.parse(saveData);
+
+    if (saveData) {
+        console.log(saveData);
+
+        refs.form.value = saveData.email;
+        refs.form.value = saveData.message;
+    }
+}
