d1d55de6e19abd9d88b2a600a91e5c1ffe28d890
DenisVoron <Denis.voronko@gmail.com>
2022-08-11 22:48:43 +0300

    made task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -11,44 +11,32 @@ const FormData = {};
 refs.form.addEventListener('submit', onFormSabmit);
 refs.form.addEventListener('input', throttle(onDataInput, 500));
 
-
-refs.form.addEventListener('input', (e) => {
-    //console.log(e.currentTarget.name);
-    //console.log(e.target.value);
-    FormData[e.target.name] = e.target.value;
-
-    const objectData = JSON.stringify(FormData);
-    localStorage.setItem(STORAGE_KEY, objectData)
-
-    console.log(FormData);
-})
-
 populateTextareaInput();
 
 function onFormSabmit(evt) {
     evt.preventDefault();
     evt.currentTarget.reset();
+
     console.log('Отправляем форму');
+
     localStorage.removeItem(STORAGE_KEY);
 }
 
-function onDataInput(evt) {
-    const data = evt.target.value;
-    console.log(data);
-    localStorage.setItem(STORAGE_KEY, data);
-    console.log(localStorage);
+function onDataInput(e) {
+
+    FormData[e.target.name] = e.target.value;
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(FormData));
 }
 
-
 function populateTextareaInput() {
-    const savedDAta = localStorage.getItem(STORAGE_KEY);
-    console.log(savedDAta);
-    const saveValue = JSON.parse(savedDAta);
+
+    const saveValue = JSON.parse(localStorage.getItem(STORAGE_KEY));
     console.log(saveValue);
 
-    /*if (saveValue) {
+    if (saveValue) {
 
         refs.form.email.value = saveValue.email;
         refs.form.message.value = saveValue.message;
-    }*/
+    }
 }
